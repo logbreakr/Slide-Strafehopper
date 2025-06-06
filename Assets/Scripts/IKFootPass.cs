aea8cfe59f66e2625da7b20fd5a4be9b7dce1192
logbreakr <126130228+logbreakr@users.noreply.github.com>
2025-06-05 23:37:30 -0400

    Update IKFootPass.cs

--- Assets/Scripts/IKFootPass.cs
+++ Assets/Scripts/IKFootPass.cs
@@ -76,48 +76,47 @@ public class IKFootPass : MonoBehaviour
                     animator.SetFloat("IKLeftFootWeight", 1f / hitL.distance);
                 }
             }
-        }
 
-        // Right Foot
-        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, animator.GetFloat("IKRightFootWeight"));
-        animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, animator.GetFloat("IKRightFootWeight"));
-        animator.SetIKHintPositionWeight(AvatarIKHint.RightKnee, 1f);
+            // Right Foot
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, animator.GetFloat("IKRightFootWeight"));
+            animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, animator.GetFloat("IKRightFootWeight"));
+            animator.SetIKHintPositionWeight(AvatarIKHint.RightKnee, 1f);
 
-        RaycastHit hitR;
-        Ray rayR = new Ray(animator.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
+            RaycastHit hitR;
+            Ray rayR = new Ray(animator.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
 
-        bool rFloored = Physics.Raycast(rayR, out hitR, distanceToGround + 1f, layerMask);
-        if (rFloored)
-        {
-            if (hitR.transform.tag == "walkable")
+            bool rFloored = Physics.Raycast(rayR, out hitR, distanceToGround + 1f, layerMask);
+            if (rFloored)
             {
-                Vector3 forward = Vector3.ProjectOnPlane(-rFoot.transform.forward, hitR.normal);
-                Vector3 footPosition = hitR.point;
-                footPosition.y += distanceToGround;
+                if (hitR.transform.tag == "walkable")
+                {
+                    Vector3 forward = Vector3.ProjectOnPlane(-rFoot.transform.forward, hitR.normal);
+                    Vector3 footPosition = hitR.point;
+                    footPosition.y += distanceToGround;
 
-                animator.SetIKHintPosition(AvatarIKHint.RightKnee, rKnee.transform.position);
+                    animator.SetIKHintPosition(AvatarIKHint.RightKnee, rKnee.transform.position);
 
-                animator.SetIKPosition(AvatarIKGoal.RightFoot, footPosition);
+                    animator.SetIKPosition(AvatarIKGoal.RightFoot, footPosition);
 
-                animator.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.LookRotation(forward, hitR.normal));
+                    animator.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.LookRotation(forward, hitR.normal));
 
-                animator.SetFloat("IKRightFootWeight", 1f / hitR.distance);
+                    animator.SetFloat("IKRightFootWeight", 1f / hitR.distance);
+                }
             }
+            // FIX, current has a ray for each foot starting at a fixed y pos to get the ground distance at each foot
+
+            Ray rayLDist = new Ray(new Vector3(lFoot.transform.position.x, 1f, lFoot.transform.position.z), Vector3.down);
+            Ray rayRDist = new Ray(new Vector3(rFoot.transform.position.x, 1f, rFoot.transform.position.z), Vector3.down);
+
+            RaycastHit rayLDistHit;
+            RaycastHit rayRDistHit;
+
+            Physics.Raycast(rayLDist, out rayLDistHit, Mathf.Infinity, layerMask);
+            Physics.Raycast(rayRDist, out rayRDistHit, Mathf.Infinity, layerMask);
+
+            bodyDisp = Mathf.Abs(rayLDistHit.distance - rayRDistHit.distance) / 2f;
+            transform.localPosition = bodyRootPos - (Vector3.up * bodyDisp);
         }
-        // FIX, current has a ray for each foot starting at a fixed y pos to get the ground distance at each foot
-
-        Ray rayLDist = new Ray(new Vector3(lFoot.transform.position.x, 1f, lFoot.transform.position.z), Vector3.down);
-        Ray rayRDist = new Ray(new Vector3(rFoot.transform.position.x, 1f, rFoot.transform.position.z), Vector3.down);
-
-        RaycastHit rayLDistHit;
-        RaycastHit rayRDistHit;
-
-        Physics.Raycast(rayLDist, out rayLDistHit, Mathf.Infinity, layerMask);
-        Physics.Raycast(rayRDist, out rayRDistHit, Mathf.Infinity, layerMask);
-
-        bodyDisp = Mathf.Abs(rayLDistHit.distance - rayRDistHit.distance) / 2f;
-        transform.localPosition = bodyRootPos - (Vector3.up * bodyDisp);
-        
     }
         
 }
